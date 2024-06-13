d823f640ca547433827b030362c4e47283f186f1
junihoj <ezeonyekachukwu98@gmail.com>
2024-06-13 17:02:07 +0100

    added strict cors

--- index.js
+++ index.js
@@ -38,10 +38,43 @@ mongoose.connect(process.env.DATABASE, {
 // 	console.log(err);
 // });
 
+const whitelist = [`${process.env.CLIENT_URL}`]
+const  corsOptions = {
+    origin: function (origin, callback) {
+      if (whitelist.indexOf(origin) !== -1 || !origin) {
+        callback(null, true)
+      } else {
+        callback(new Error('Not allowed by CORS'))
+      }
+    }
+}
 
 
 //apply middlewares
-app.use(cors());
+app.use(cors(corsOptions));
+
+// app.use((req, res, next) => {
+//     res.setHeader(
+//       "Access-Control-Allow-Origin",
+//       `${process.env.CLIENT_URL}`
+//     );
+//     res.setHeader(
+//       "Access-Control-Allow-Methods",
+//       "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
+//     );
+//     res.setHeader(
+//       "Access-Control-Allow-Headers",
+//       "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
+//     );
+//     res.setHeader("Access-Control-Allow-Credentials", true);
+//     res.setHeader("Access-Control-Allow-Private-Network", true);
+//     //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
+//     res.setHeader("Access-Control-Max-Age", 7200);
+  
+//     next();
+// });
+
+
 
 app.use(express.json({limit:'5mb'}));
 
@@ -57,7 +90,10 @@ readdirSync('./routes').map((r)=>
 // });
 
 
-
+app.get("/healthz", (req, res) => {
+    console.log("health check is processed");
+    return res.status(204).send();
+});
 const port = process.env.PORT || 8000;
 
 app.listen(port, ()=>console.log(`Server is running on port ${port}`));
